01f6c4a54edee94ec64ae24118f31deaf70c7e11
sigarachi <lalalabtv@gmail.com>
2024-02-14 21:42:48 +0300

    Error fix

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -14,35 +14,53 @@ export const Home = (): React.ReactElement => {
 		return <>Статистика не загрузилась :(</>;
 	}
 
+	if (!data?.data?.bots.length) {
+		return (
+			<Flex width="100%" height="100%" align="center" justify="center">
+				Тут будет отображаться текущий матч
+			</Flex>
+		);
+	}
+
 	return (
 		<Flex width="100%" align="center" direction="column">
 			<p>Текущий раунд</p>
-			<p>ID: {data?.data?.id}</p>
-			<p>Сыграно раундов: {data?.data?.roundInfo.length}</p>
+			<p>ID: {data && data.data && data?.data?.id}</p>
+			<p>
+				Сыграно раундов: {data && data.data && data?.data?.roundInfo.length}
+			</p>
 			<Flex gap="24px" pd="10px 0">
 				<User
-					name={data?.data?.bots[0].title}
-					description={`Позиция: ${data?.data?.bots[0].position}`}
+					name={data?.data?.bots[0]?.title || 'Пока нет соперника'}
+					description={`Позиция: ${data?.data?.bots[0]?.position || 'Пока нет рейтинга'}`}
 				/>
 				<Flex gap="5px">
-					{data?.data?.roundInfo.map((item, index) => (
-						<Flex direction="column" align="center">
-							<p className="text-xs">Раунд: {index + 1}</p>
-							<p className="text-xs">
-								Статус: {item.isPlay ? 'В процессе' : 'Завершен'}
-							</p>
-							<p className="text-xs">
-								Побед {data?.data?.bots[0].title}: {item.topBotWinCount}
-							</p>
-							<p className="text-xs">
-								Побед {data?.data?.bots[1].title}: {item.downBotWinCount}
-							</p>
-						</Flex>
-					))}
+					{data &&
+						data.data &&
+						data?.data?.roundInfo?.map((item, index) => (
+							<Flex direction="column" align="center">
+								<p className="text-xs">Раунд: {index + 1}</p>
+								<p className="text-xs">
+									Статус: {item.isPlay ? 'В процессе' : 'Завершен'}
+								</p>
+								<p className="text-xs">
+									Побед {data?.data?.bots[0]?.title}: {item.topBotWinCount}
+								</p>
+								<p className="text-xs">
+									Побед {data?.data?.bots[1]?.title}: {item.downBotWinCount}
+								</p>
+							</Flex>
+						))}
 				</Flex>
 				<User
-					name={data?.data?.bots[1].title}
-					description={`Позиция: ${data?.data?.bots[1].position}`}
+					name={
+						(data && data.data && data?.data?.bots[1]?.title) ||
+						'Пока нет соперника'
+					}
+					description={`Позиция: ${
+						(data && data.data && data?.data?.bots[1]?.position) ||
+						'Пока нет рейтинга'
+					}`}
 				/>
 			</Flex>
 		</Flex>
--- src/pages/statistic/statistic.tsx
+++ src/pages/statistic/statistic.tsx
@@ -1,3 +1,4 @@
+import { Flex } from '@components/flex';
 import {
 	CircularProgress,
 	Table,
@@ -22,38 +23,45 @@ export const Statistic = (): React.ReactElement => {
 	}
 
 	return (
-		<Table>
-			<TableHeader>
-				<TableColumn>ID</TableColumn>
-				<TableColumn>Бот 1</TableColumn>
-				<TableColumn>Бот 2</TableColumn>
-				<TableColumn>Количество побед 1</TableColumn>
-				<TableColumn>Количество побед 2</TableColumn>
-				<TableColumn>Победитель</TableColumn>
-			</TableHeader>
-			<TableBody>
-				{(data?.data &&
-					data?.data?.map((item) => {
-						return (
-							<TableRow key={item.id}>
-								<TableCell>{item.id}</TableCell>
-								<TableCell>{item.botTitleByPosition['1']}</TableCell>
-								<TableCell>{item.botTitleByPosition['2']}</TableCell>
-								<TableCell>{item.result[0].topBotWinCount}</TableCell>
-								<TableCell>{item.result[0].downBotWinCount}</TableCell>
-								<TableCell>
-									{item.result[0].isDownBotWin
-										? item.botTitleByPosition['2']
-										: item.botTitleByPosition['1']}
-								</TableCell>
+		<>
+			{(data && data?.data?.length && (
+				<Table>
+					<TableHeader>
+						<TableColumn>ID</TableColumn>
+						<TableColumn>Бот 1</TableColumn>
+						<TableColumn>Бот 2</TableColumn>
+						<TableColumn>Количество побед 1</TableColumn>
+						<TableColumn>Количество побед 2</TableColumn>
+						<TableColumn>Победитель</TableColumn>
+					</TableHeader>
+					<TableBody>
+						{data?.data?.map((item) => {
+							return (
+								<TableRow key={item?.id}>
+									<TableCell>{item?.id}</TableCell>
+									<TableCell>{item?.botTitleByPosition['1']}</TableCell>
+									<TableCell>{item?.botTitleByPosition['2']}</TableCell>
+									<TableCell>{item?.result[0]?.topBotWinCount}</TableCell>
+									<TableCell>{item?.result[0]?.downBotWinCount}</TableCell>
+									<TableCell>
+										{item?.result[0]?.isDownBotWin
+											? item?.botTitleByPosition['2']
+											: item?.botTitleByPosition['1']}
+									</TableCell>
+								</TableRow>
+							);
+						}) || (
+							<TableRow key={'1'}>
+								<TableCell>1</TableCell>
 							</TableRow>
-						);
-					})) || (
-					<TableRow key={'1'}>
-						<TableCell>1</TableCell>
-					</TableRow>
-				)}
-			</TableBody>
-		</Table>
+						)}
+					</TableBody>
+				</Table>
+			)) || (
+				<Flex width="100%" height="100%" align="center" justify="center">
+					Тут будет Статистика
+				</Flex>
+			)}
+		</>
 	);
 };
